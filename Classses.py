00b02de40c7bafb76d83ae52e8c8f82375c7c81c
Scyyyuu <cyyyuu65@gmail.com>
2026-09-20 22:59:39 +0800

    Add files via upload

---
+++ Classses.py
@@ -0,0 +1,13 @@
+class Gamer:
+    def __init__(self, name):
+        self.player = name
+        self.health = 100
+        self.score = 0
+
+player1= Gamer ("Alex")
+player2= Gamer ("Cyrus")
+player3= Gamer ("De Villa")
+player4= Gamer ("Paul")
+
+for x in range (1,11):
+    print (player3)
---
+++ Def.py
@@ -0,0 +1,8 @@
+# 1. Define the function with parameters 'a' and 'b'
+def add_numbers(a, b):
+    sum_result = a + b
+    return sum_result
+
+# 2. Call the function and save the output
+total = add_numbers(5, 10)
+print(total)
