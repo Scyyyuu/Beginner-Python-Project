df1c67d0b89c66296a4496a2025060698f34a9a3
Scyyyuu <cyyyuu65@gmail.com>
2026-09-21 23:37:23 +0800

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+Anime = {"Frieren": "Kanehito Yamada",
+         "Attack On Titan": "Hajime Isayama",
+         "Love Is War": "Aka Akasaka",
+         "Code Geass": "Ichirō Ōkouchi",
+         "Steins; Gate": "Chiyomaru Shikura",
+         "Oshi No Ko": "Aka Akasaka",
+         "Haikyuu": "Haruichi Furudate"
+         }
+
+for x in range (1,11):
+    print (Anime.get ("Love Is War"))
+    print (Anime.get ("Attack On Titan"))
+    print (Anime.get ("Code Geass"))
+    print (Anime.get ("Oshi No Ko"))
+    print (Anime.get ("Haikyuu"))
+    print (x)
